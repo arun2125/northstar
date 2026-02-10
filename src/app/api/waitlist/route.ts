f223da9a3881e92eb3ad9e1846136227d26c1363
Arun Yadav <arun2125@users.noreply.github.com>
2026-02-10 06:50:39 +0530

    Update route.ts

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
