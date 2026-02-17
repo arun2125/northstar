ebf72591f4d41914b45b446f46761cf37036c55e
primetechgov <arun@primetechgov.com>
2026-02-17 14:13:56 +0530

    Remove redirect rule - causing redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.northstarastro.com',
-          },
-        ],
-        destination: 'https://northstarastro.com/:path*',
-        permanent: true, // 301 redirect
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
